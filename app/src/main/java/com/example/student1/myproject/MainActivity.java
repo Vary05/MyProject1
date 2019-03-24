e8c5292dc684c644caaada58f6c7961a61a7a667
vary05 <varypetra@gmail.com>
2019-03-24 16:55:02 +0300

    01

--- app/src/main/java/com/example/student1/myproject/MainActivity.java
+++ app/src/main/java/com/example/student1/myproject/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.student1.myproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main)
+        next=findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),LoginSignActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
